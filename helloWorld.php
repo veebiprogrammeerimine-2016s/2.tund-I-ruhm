777077452960911be49684520d3829404f8b68fe
Romil Robtsenkov <romil@greeny.cs.tlu.ee>
2016-09-14 09:34:04 +0300

    added if statement, for loop and date

--- helloWorld.php
+++ helloWorld.php
@@ -10,3 +10,47 @@
 	echo $firstName." ".$lastName;
 	
 ?>
+
+<br>
+
+<?php 
+	
+	$age = 5;
+		
+	if ( $age < 18 ) {
+		
+		// kui vastus on tõene (true)
+		echo "alaealine";
+		
+	} else {
+		
+		// kui vastus on väär (false)
+		echo "täisealine";
+		
+	}
+	
+?>
+
+<br>
+
+<?php 
+
+
+	for ($i = 0; $i < $age; $i = $i + 1) {
+		
+		echo $i."palju ";
+		
+	}
+	
+	echo "õnne";
+
+?>
+
+<br>
+
+<?php 
+
+	echo date("d.Y");
+
+?>
+
