5f4200ed06fed5bd141bb8963d49c67723da3cbe
Romil Robtsenkov <romil@greeny.cs.tlu.ee>
2016-09-14 08:53:22 +0300

    first lines of code

---
+++ helloWorld.php
@@ -0,0 +1,12 @@
+<?php
+
+	$firstName = "Romil";
+	$lastName = "Rõbtšenkov";
+
+	// echo trükib välja muutuja väärtuse
+	//echo $firstName;
+	//echo $lastName;
+	
+	echo $firstName." ".$lastName;
+	
+?>
